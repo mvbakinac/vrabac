7a70a928a6e94784af9db6f36018a2047efe5912
Milo Vrach <morelo87@gmail.com>
2019-03-18 10:45:54 +0100

    Add a functionality to like and unlike a message.

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -5,4 +5,12 @@ module ShoutsHelper
     gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
     image_tag(gravatar_url)
   end
+
+  def like_button(shout)
+     if current_user.has_liked?(shout)
+       button_to "Unlike", unlike_shout_path(shout), method: :delete
+     else
+       button_to "Like", like_shout_path(shout), method: :post
+     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  get 'shouts/create'
-  get 'dashboards/show'
+
+
   constraints Clearance::Constraints::SignedIn.new do
      root to: 'dashboards#show'
   end
+
   root to: 'homes#show'
-  resources :shouts, only: [:create, :show]
+
+  resources :shouts, only: [:create, :show] do
+    member do
+      post "/like" => "likes#create"
+      delete "/unlike" => "likes#destroy"
+    end
+  end
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
